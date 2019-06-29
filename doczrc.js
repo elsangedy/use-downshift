8aaf52decf60fd58f0bfcd05d6f5dd4505f7a68e
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2019-06-29 02:16:23 -0300

    docs: add repository in docz

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,12 @@
 export default {
   dest: '/dist',
   codeSandbox: false,
-  menu: ['Home', { name: 'Examples', menu: ['Dropdown', 'Autocomplete', 'Typeahead', 'MultiSelect'] }]
+  repository: 'https://github.com/elsangedy/use-downshift',
+  menu: [
+    'Home',
+    {
+      name: 'Examples',
+      menu: ['Dropdown', 'Autocomplete', 'Typeahead', 'MultiSelect'],
+    },
+  ],
 }
